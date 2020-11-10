cc381f50e79b45c203c79d659dbea1767cff7e99
Tyler Bach <Tbach2@my.wctc.edu>
2020-11-10 01:27:00 -0800

    option 4 updated

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace BlogsConsole
                 userInput = Console.ReadLine();
                 logger.Info("Option {userInput} selected", userInput);
 
-                // Display all Blogs from the database
+                // Display Blogs
                 if(userInput == "1")
                 {     
                     var blogs = db.Blogs.OrderBy(b => b.Name);
@@ -34,7 +34,7 @@ namespace BlogsConsole
                     foreach(var item in blogs){ Console.WriteLine(item.Name); }
                 }
 
-                // Create and save a new Blog
+                // Create Blog
                 else if(userInput == "2")
                 {
                     Console.Write("Enter a name for a new Blog: ");
@@ -48,6 +48,7 @@ namespace BlogsConsole
                     else{ logger.Error("Blog name cannot be null"); }
                 }
 
+                //Create Post
                 else if(userInput == "3")
                 {
                     if(db.Blogs.Count() != 0)
@@ -63,11 +64,11 @@ namespace BlogsConsole
                             if(blogId > db.Blogs.Count()){ logger.Error("There are no Blogs saved with that Id"); }
                             else 
                             {
-                                Console.WriteLine("Enter the post title: ");
+                                Console.WriteLine("Enter the Post title");
                                 string title = Console.ReadLine();
                                 if (title != "")
                                 {
-                                    Console.WriteLine("Enter the post content:");
+                                    Console.WriteLine("Enter the Post content");
                                     string content = Console.ReadLine();
                                     var post = new Post{ Title = title, Content = content, BlogId = blogId };
 
@@ -81,9 +82,40 @@ namespace BlogsConsole
                     }else{ Console.WriteLine(db.Blogs.Count() + " Blogs returned"); }
                 }
 
+                //Display Posts
                 else if(userInput == "4")
-                {}
-
+                {
+                    if(db.Blogs.Count() != 0)
+                    {
+                        int blogId = 0; 
+                        Console.WriteLine("Select the blog's posts to display:");
+                        var blogs = db.Blogs.OrderBy(b => b.BlogId);
+                        Console.WriteLine("0) Posts from all blogs");
+                        foreach(var item in blogs){ Console.WriteLine(item.BlogId + ")" + " Posts from " + item.Name); }
+                        try
+                        {
+                            blogId = Int32.Parse(Console.ReadLine());
+                            if(blogId > db.Blogs.Count()){ logger.Error("There are no Blogs saved with that Id"); }
+                            else
+                            {
+                                if(blogId == 0)
+                                {
+                                    Console.WriteLine(db.Posts.Count() + " post(s) returned");
+                                    foreach(var item in db.Posts)
+                                    { Console.WriteLine("Blog: " + item.Blog.Name + "\nTitle: " + item.Title + "\nContent: " + item.Content); }
+                                }
+                                else
+                                {  
+                                    var posts = db.Posts.Where(p => p.BlogId == blogId);
+                                    Console.WriteLine(posts.Count() + " post(s) returned");
+                                    foreach(var item in posts)
+                                    { Console.WriteLine("Blog: " + item.Blog.Name + "\nTitle: " + item.Title + "\nContent: " + item.Content); }
+                                }
+                            }
+                        }catch(FormatException){ logger.Error("Invalid Blog Id"); }
+                    }
+                    else{ Console.WriteLine(db.Blogs.Count() + " Blogs returned"); }
+                }
                 Console.WriteLine();//spacer
           
             }while(userInput != "q");
